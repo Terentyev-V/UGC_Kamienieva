0293cd58edca7c79f6b1bf8c1a4521a792c3f23c
BlackT <terentyevBlackthorn@gmail.com>
2024-11-15 17:33:59 +0100

    Offset responsive corrections

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-scroll";
 
 
 
 export default function Header () {
+
+    const [offset, setOffset] = useState(-30); // Default offset
+
+  useEffect(() => {
+    const updateOffset = () => {
+      if (window.innerWidth <= 480) {
+        setOffset(-70); // offset for small screens
+      } else if ((window.innerWidth <= 1370))
+        setOffset(10); 
+      else {
+        setOffset(-30); // Default offset for larger screens
+      }
+    };
+
+    updateOffset();
+
+    // Update the offset whenever the window is resized
+    window.addEventListener('resize', updateOffset);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', updateOffset);
+    };
+  }, []);
   
     return (
         <>
@@ -17,15 +41,23 @@ export default function Header () {
                         to="aboutMe"
                         smooth={true}
                         duration={500}
-                        offset={-70} >
+                        offset={offset} >
                     About Me
                     </Link>
 
-                    <Link to="gallery" smooth={true} duration={500} offset={-70}>
+                    <Link 
+                    to="gallery" 
+                    smooth={true} 
+                    duration={500} 
+                    offset={10}>
                     Gallery
                     </Link>
 
-                    <Link to="contacts" smooth={true} duration={500} offset={-70}>
+                    <Link 
+                    to="contacts" 
+                    smooth={true} 
+                    duration={500} 
+                    offset={-70}>
                     Contacts
                     </Link>
                 </div>
--- src/components/Section2.js
+++ src/components/Section2.js
@@ -1,26 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 import PhotoAbout from '../images/PhotoAbout.jpg';
-
+import elipse from "../images/elipse.svg"
 export default function Section2() {
-  
-    return (
-        <div className="section2Out">
-            <section id="aboutMe" className="section2">        
-                <div className="section2Left">
-                    <h1> It’s just Me, Myself and I </h1>
-                    <h2>
-                        I am a passionate UGC creator at the beginning of my journey, driven by the desire to connect brands with their audiences through authentic and engaging content. With a fresh perspective and a creative spark, I love capturing real moments that inspire and resonate with people. 
-                        <br /><br />
-                        My goal is to bring a unique voice and genuine storytelling to every project, helping brands build meaningful relationships with their communities. 
-                        <br /><br />
-                        I believe that every piece of content has the power to make a difference, and I am excited to be a part of that journey.
-                    </h2>
-                </div>
-                <div className="section2Right">
-                    <img className="photoAbout" src={PhotoAbout} alt="Portrait Kamienieva Tetiana at the sea"/>
-                </div>
-            </section>
-        </div>
-    );
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  function toggleText() {
+    setIsExpanded(!isExpanded);
   }
+
+  return (
+    <div className="section2Out">
+      <section id="aboutMe" className="section2 ">        
+        <div className="section2Left">
+          <h1> It’s just Me, Myself and I </h1>
+          <h2 onClick={toggleText} className={isExpanded ? "full-text" : "multiline-truncated-text"} >
+            I am a passionate UGC creator at the beginning of my journey, driven by the desire to connect brands with their audiences through authentic and engaging content. With a fresh perspective and a creative spark, I love capturing real moments that inspire and resonate with people.
+            <br /><br />
+            My goal is to bring a unique voice and genuine storytelling to every project, helping brands build meaningful relationships with their communities. 
+            <br /><br />
+            I believe that every piece of content has the power to make a difference, and I am excited to be a part of that journey.
+          </h2>
+        </div>
+        <div className="section2Right">
+          <img className="photoAbout" src={PhotoAbout} alt="Portrait Kamienieva Tetiana at the sea" />
+        </div>
+      </section>
+    </div>
+  );
+}
